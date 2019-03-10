48bcc296f2cd29158f999883869392f2456d6195
Ruslan <plexus.9310@gmail.com>
2019-03-10 16:58:35 -0700

    Add tests for Tool numbers

---
+++ test/test_tool_numbers.c
@@ -0,0 +1,119 @@
+/**
+  ******************************************************************************
+  * @file    test_tool_numbers.c
+  * @author  
+  * @version V0.0.1
+  * @date    
+  * @brief   This file provides tests for tool_numbers.
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE IS FOR EXAMPLE ONLY AIMS. AS A RESULT, AUTHOR SHALL
+  * NOT BE HELD LIABLE FOR ANY DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH
+  * RESPECT TO ANY CLAIMS ARISTING FROM THE CONTENT OF SUCH FIRMWARE AND/OR
+  * THE USE MADE BY CUSTOMERS OF THE CODING INFORMATIONCONTAINED HEREIN IN
+  * CONNECTION WITN THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; Author </center></h2>
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "unity.h"
+#include "tool_numbers.h"
+
+/** @addtogroup Tool_numbers_module
+  * @{
+  */
+
+/** @defgroup TESTS
+  * @brief Tests for Tool numbers module
+  * @{
+  */
+
+/** @defgroup TESTS_Private_TypesDefinitions
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+/** @defgroup TESTS_Private_Defines
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+/** @defgroup TESTS_Private_Macros
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+/** @defgroup TESTS_Private_Variables
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+/** @defgroup TESTS_Private_FunctionPrototypes
+  * @{
+  */
+
+/**
+  * @}
+  */
+
+/** @defgroup TESTS_Private_Functions
+  * @{
+  */
+
+
+/**
+  * @brief Per-test setup
+  * @param None
+  * @retval None
+  */
+void setUP( void )
+{
+
+}
+
+/**
+  * @brief Per-test cleanup
+  * @param None
+  * @retval None
+  */
+void tearDown( void )
+{
+
+}
+
+/**
+  * @brief
+  * @param
+  *
+  *     @arg
+  * @retval none
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/********************************* Example *********************END OF FILE****/
